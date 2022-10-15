a533dba1a0a2d7db5b591febf6eda3a736fe8783
timeansalot <timemeansalot@gmail.com>
2022-10-15 18:04:07 +0800

    sequential: shift register

---
+++ ScalaPractice/hello-world/src/main/scala/ShiftRegister.scala
@@ -0,0 +1,38 @@
+/** FILE: ShiftRegister.scala
+  *
+  * ShiftRegister for delay, parallel output
+  */
+
+import chisel3._
+
+/** output is 4 cycle delay of input
+  */
+class DelayRegister extends Module {
+  val io = IO(new Bundle {
+    val din = Input(UInt(1.W))
+    val dout = Output(UInt(1.W))
+  })
+
+  val shiftReg = Reg(UInt(4.W))
+  shiftReg := shiftReg(2, 0) ## io.din
+
+  io.dout := shiftReg(3)
+}
+
+/** change serial input to parallel output
+  */
+class ParallelRegister extends Module {
+  val io = IO(new Bundle {
+    val din = Input(UInt(1.W))
+    val dout = Output(UInt(4.W))
+  })
+
+  val parallelReg = RegInit(0.U(4.W))
+  parallelReg := io.din ## parallelReg(3, 1)
+  io.dout := parallelReg
+}
+
+object GetShiftRegisterVerilog extends App {
+//   (new chisel3.stage.ChiselStage).emitVerilog(new DelayRegister())
+  (new chisel3.stage.ChiselStage).emitVerilog(new ParallelRegister())
+}
