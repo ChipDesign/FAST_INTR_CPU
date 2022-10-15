63f2bd6611fe02fb83166eb0fb4431f8b7523bd1
timeansalot <timemeansalot@gmail.com>
2022-10-15 15:18:01 +0800

    add priority encoder

--- ScalaPractice/hello-world/src/test/scala/EncoderTest.scala
+++ ScalaPractice/hello-world/src/test/scala/EncoderTest.scala
@@ -11,7 +11,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 class EncoderTest extends AnyFlatSpec with ChiselScalatestTester {
     "EncoderGenerator" should "pass" in{
-        test(new EncoderGenerator)
+        test(new EncoderGenerator(4))
         .withAnnotations(Seq(WriteVcdAnnotation)){dut=>
             dut.io.hotIn.poke("b0001".U)
             dut.io.code.expect("b00".U)
@@ -28,6 +28,23 @@ class EncoderTest extends AnyFlatSpec with ChiselScalatestTester {
             dut.io.hotIn.poke("b1000".U)
             dut.io.code.expect("b11".U)
             dut.clock.step(1)        
+        }
+    }
+
+    "PriorityEncoder" should "pass" in{
+        test(new PriorityEncoder(4))
+        .withAnnotations(Seq(WriteVcdAnnotation)){dut=>
+            dut.io.hotIn.poke("b0000".U)
+            dut.io.code.expect("b00".U)
+            dut.clock.step(1)    
+
+            dut.io.hotIn.poke("b1111".U)
+            dut.io.code.expect("b00".U)
+            dut.clock.step(1)
+
+            dut.io.hotIn.poke("b0100".U)
+            dut.io.code.expect("b10".U)
+            dut.clock.step(1)
         }
     }
 }
