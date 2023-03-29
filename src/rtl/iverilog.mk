f3f59246ceffb939296b1460d1dd3ab77f577846
timemeansalot <timemeansalot@gmail.com>
2023-03-29 15:35:58 +0800

    config iverilog

---
+++ src/rtl/iverilog.mk
@@ -0,0 +1,14 @@
+.PHONY: clean all run waveform
+
+DEFAULT_GOAL:=all
+all: ${src}.v ${src}_tb.v
+	@iverilog -o ${src}_tb.vvp ${src}_tb.v # @ means don't echo this command to terminal, just run it.
+
+run: all 
+	vvp ${src}_tb.vvp
+
+waveform: run
+	@gtkwave ${src}_tb.vcd
+
+clean:
+	-rm *vcd *vvp
