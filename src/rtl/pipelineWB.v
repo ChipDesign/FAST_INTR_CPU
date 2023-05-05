1dddc8e156fc10d77eb90e72422c6e0e9f201ac0
timemeansalot <timemeansalot@gmail.com>
2023-05-05 11:23:34 +0800

    add imm and pc+4 as wb source

--- src/rtl/pipelineWB.v
+++ src/rtl/pipelineWB.v
@@ -1,38 +1,59 @@
 `ifndef __PIPELINEWB__
 `define __PIPELINEWB__
+/*
+file: 5 stage pipelne write back stage
+    select one of the four write back source, 
+    pass the select source to RF(register file) in ID(instruction decode) stage
+author: fujie 
+time: 2023年 5月 5日 星期五 11时06分55秒 CST
+*/
+`include "definitions.vh"
 module pipelineWB (
   input wire clk,
   input wire resetn,
   
-  input wire regWriteEnM,
-  input wire [1:0] resultSrcM,
-  input wire [31:0] aluResultM,
-  input wire [31:0] memReadDataM, // delared as wire, becased the D-memory has 1 cycle delay when reading
-  input wire [4:0] rdM,
+  input wire regWriteEnM_i,
+  input wire [1:0] resultSrcM_i,    // select signal to choose one of the four inputs
+  input wire [31:0] aluResultM_i,   // alu calculation result
+  input wire [31:0] memReadDataM_i, // delared as wire, becased the D-memory has 1 cycle delay when reading
+  input wire [31:0] pcPlus4_i,      // rd=pc+4, for `jal` instruction
+  input wire [31:0] extendedImm_i,  // extended imm, for 'lui' instruction
+  input wire [4:0] rdM_i,  // RF write back register index, passed from MEM stage
 
-  output reg regWriteEnW,
-  output reg [4:0] rdW,
-  output reg [31:0] writeBackDataW
+  output reg regWriteEnW_o, // write back to RF enable
+  output reg [4:0] rdW_o,   // RF write register index
+  output reg [31:0] writeBackDataW_o // data write to RF in ID 
 );
 
 
+// =========================================================================
+// ============================ implementation =============================
+// =========================================================================
 always@(posedge clk)begin 
-  if(~resetn)begin
-    regWriteEnW <= 0;
-    rdW <=0;
-    writeBackDataW <= 0;
-  end
-  else begin 
-    regWriteEnW <= regWriteEnM;
-    rdW <= rdM;
-    if(resultSrcM === 2'b00) begin  // alu for resutl
-      writeBackDataW <= aluResultM;
+    if(~resetn)begin
+        regWriteEnW_o <= 0;
+        rdW_o <=0;
+        writeBackDataW_o <= 0;
     end
-    else begin  // load/store for result
-      writeBackDataW <= memReadDataM;
+    else begin 
+        regWriteEnW_o <= regWriteEnM_i;
+        rdW_o <= rdM_i;
+        case(resultSrcM_i) 
+            `WBSRC_ALU: begin
+                writeBackDataW_o <= aluResultM_i; // choose alu result to write back
+            end
+            `WBSRC_IMM: begin
+                writeBackDataW_o <= extendedImm_i; // choose extended immdiate to write back
+            end
+            `WBSRC_MEM: begin
+                writeBackDataW_o <= memReadDataM_i;// choose data memory output to write back
+            end
+            `WBSRC_PC: begin
+                writeBackDataW_o <= pcPlus4_i;// choose pc+4 to write back
+            end
+            default: writeBackDataW_o <= aluResultM_i;
+        endcase
     end
-  end
-
 end
 endmodule
 `endif
