0d04d40dae584a7237bb62ed41d25b4c3a05533e
timemeansalot <timemeansalot@gmail.com>
2023-08-09 17:32:11 +0800

    add m and c tests

--- src/verification/riscvtest/revise.sh
+++ src/verification/riscvtest/revise.sh
@@ -1,9 +1,10 @@
 #/bin/bash
 path=$1
-files=$(ls $path )
+files=$(ls $path |grep .S )
 for filename in $files
 do
-   # echo $filename
-   sed -i 's/#RVTEST_RV64U/\#RVTEST_RV64U/' $filename
-   sed -i 's/#RVTEST_CODE_BEGIN/\#RVTEST_CODE_BEGIN/' $filename
+   echo $filename
+   sed -i 's/RVTEST_RV64U/\#RVTEST_RV64U/' $filename
+   sed -i 's/RVTEST_RV32U/\#RVTEST_RV32U/' $filename
+   sed -i 's/RVTEST_CODE_BEGIN/\#RVTEST_CODE_BEGIN/' $filename
 done
