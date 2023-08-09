8b2ca45c8f1900c71cf17dcb126ece8e02c96d76
timemeansalot <timemeansalot@gmail.com>
2023-08-09 17:34:33 +0800

    add m and c tests, remove csr part

--- src/verification/riscvtest/revise.sh
+++ src/verification/riscvtest/revise.sh
@@ -4,7 +4,7 @@ files=$(ls $path |grep .S )
 for filename in $files
 do
    echo $filename
-   sed -i 's/RVTEST_RV64U/\#RVTEST_RV64U/' $filename
-   sed -i 's/RVTEST_RV32U/\#RVTEST_RV32U/' $filename
-   sed -i 's/RVTEST_CODE_BEGIN/\#RVTEST_CODE_BEGIN/' $filename
+   sed -i 's/^RVTEST_RV64U/\#RVTEST_RV64U/' $filename
+   sed -i 's/^RVTEST_RV32U/\#RVTEST_RV32U/' $filename
+   sed -i 's/^RVTEST_CODE_BEGIN/\#RVTEST_CODE_BEGIN/' $filename
 done
