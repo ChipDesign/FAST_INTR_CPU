6ecb944203c5ec016e7e93d668746c79ea66ad14
timemeansalot <timemeansalot@gmail.com>
2023-08-09 15:47:04 +0800

    remove verification folder which is using iverilog

--- src/verification/rtl/cgen.v
+++
@@ -1,9 +0,0 @@
-module cgen(
-ain,bin,cin,cout);
-
-input ain,bin,cin;
-output cout;
-
-assign cout=ain?cin:bin;
-
-endmodule
--- src/verification/rtl/simpleDUT.v
+++
@@ -1,9 +0,0 @@
-module simpleDUT (
-  input wire in,
-  output wire out
-);
-
-  reg [31:0] memory[15:0]; // a 16*32 memory
-  assign out=in+1;
-  
-endmodule
